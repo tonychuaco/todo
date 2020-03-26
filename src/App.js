c7c7a7345058f8133a5db3c017dbbaa71a0f4db3
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2020-03-25 20:51:49 -0400

    added form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ToDoList extends Component {
+  constructor () {
+    super();
+    this.state = {
+      todos: [],
+      currentToDo: "",
+    };
+  }
+
+  addItem = (event) => {
+    event.preventDefault();
+    console.log("addItem method executed");
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.addItem}>
+          <label htmlFor="taskName">Task Name:</label>
+          <input onChange={this.handleChange} name="taskName" type="text" placeholder="Add to do here!" />
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default ToDoList;
