2f1d095dab000a63be01f8a5d8e1e77693b408a7
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2020-03-31 17:16:16 -0400

    prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 class ToDoList extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       todos: [],
@@ -12,22 +12,27 @@ class ToDoList extends Component {
   addItem = (event) => {
     event.preventDefault();
     console.log("addItem method executed");
-  }
+  };
 
   handleChange = (event) => {
     console.log(event.target.value);
-  }
+  };
 
   render() {
     return (
       <div className="container">
         <form onSubmit={this.addItem}>
           <label htmlFor="taskName">Task Name:</label>
-          <input onChange={this.handleChange} name="taskName" type="text" placeholder="Add to do here!" />
+          <input
+            onChange={this.handleChange}
+            name="taskName"
+            type="text"
+            placeholder="Add to do here!"
+          />
           <button type="submit">Add Task</button>
         </form>
       </div>
-    )
+    );
   }
 }
 
