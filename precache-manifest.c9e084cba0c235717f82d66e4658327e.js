a0e61c49eea96c82b234af73014762ad3164ceb9
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2020-04-01 21:00:15 -0400

    Updates

---
+++ precache-manifest.c9e084cba0c235717f82d66e4658327e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b134e940471a71ce2ed4e8455034880",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "127a0626c305329b74f9",
+    "url": "/todo/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d3572d982003f68dba29",
+    "url": "/todo/static/js/2.652d8338.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.652d8338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "127a0626c305329b74f9",
+    "url": "/todo/static/js/main.172bde54.chunk.js"
+  },
+  {
+    "revision": "10fd3631c43d335a3db9",
+    "url": "/todo/static/js/runtime-main.2a8ec137.js"
+  }
+]);
